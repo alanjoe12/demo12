0c845ffbb5f4104b6dbffdcf54dfa56f0d9ab0b9
Alan Joe Anil <71687535+alanjoe12@users.noreply.github.com>
2022-07-26 09:55:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template, request,jsonify
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
+# ‘/’ URL is bound with hello_world() function.
 def Home():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True,port=1234)
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
